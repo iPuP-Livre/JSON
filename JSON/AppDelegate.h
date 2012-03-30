d6158eef0b292a43b5466c645ccd21bd34aa5c95
ipodishima <marian.paul2@gmail.com>
2012-03-30 14:49:45 +0200

    Initial Commit

---
+++ JSON/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSON
+//
+//  Created by Marian Paul on 30/03/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
